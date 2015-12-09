2ab3b0b24902a3309b989bb30e42f71cd7814a41
Nicolas Godfraind <nicolas.godfraind@gmail.com>
2015-12-09 17:09:38 +0100

    More fixes.

--- ClarolineAngularDataTableBundle.php
+++ ClarolineAngularDataTableBundle.php
@@ -3,8 +3,18 @@
 namespace Claroline\AngularDataTableBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class ClarolineAngularDataTableBundle extends Bundle
+class ClarolineAngularDataTableBundle extends Bundle implements AutoConfigurableInterface
 {
+    public function supports($environment)
+    {
+        return true;
+    }
 
+    public function getConfiguration($environment)
+    {
+        return new ConfigurationBuilder();
+    }
 }
